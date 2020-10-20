b74a44f074521eff80caaa161c5fb92a64a24882
EricDiGi <etd7@students.uwf.edu>
2020-10-20 13:42:11 -0500

    friend op modified

--- SingleLinked.hpp
+++ SingleLinked.hpp
@@ -12,6 +12,14 @@ class SingleLinked{
         void append(int ndat);
         //void push(Node** ref, int ndat);
         //void insertAfter(Node* prev, int ndat);
+        friend std::ostream &operator<< (std::ostream &out, SingleLinked &L){
+            while(L.list != NULL){
+                out << L.list->data << " ";
+                L.list = L.list->next;
+            }
+            out << std::endl;
+            return out;
+        }
 };
 
 
--- testBench.cpp
+++ testBench.cpp
@@ -8,4 +8,9 @@ int main(){
     // This  is a testbench and demonstration of Linked-Lists
     SingleLinked list = SingleLinked();
     list.append(0);
+    list.append(1);
+    list.append(2);
+    list.append(3);
+    list.append(4);
+    cout << "My list: " << list;
 }
