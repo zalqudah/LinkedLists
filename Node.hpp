da0c9400d6c1149661fb0bac8c023360d76bd0c2
zalqudah <59494628+zalqudah@users.noreply.github.com>
2020-10-20 16:15:39 -0500

    Update Node.hpp

--- Node.hpp
+++ Node.hpp
@@ -6,6 +6,14 @@ class Node{
     public:
         int data;
         Node* next;
+    Node(){
+        this->data = 0;
+        this->next = NULL;
+    }
+    Node(int data){
+        this->data = data;
+        this->next = NULL;
+    }
 };
 
 
